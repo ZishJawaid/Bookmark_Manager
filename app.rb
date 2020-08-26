366971a513563aa47ff78bd6e0d50a977dee349f
ZishJawaid <zisheanj@gmail.com>
2020-08-26 17:43:25 +0100

    Added form and POST route. Feature test passing

--- app.rb
+++ app.rb
@@ -14,6 +14,18 @@ class BookmarkManager < Sinatra::Base
         erb :view
     end
 
+    get '/bookmarks/new' do
+        erb :new
+    end
+
+    post '/bookmarks' do
+        url = params['url']
+        connection = PG.connect(dbname: 'bookmark_manager_test')
+        connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}');")
+        redirect '/bookmarks'
+    end
+
+
 run! if app_file == $0
 end
 
